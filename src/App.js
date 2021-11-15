f74f3dbd76635d0c27a3db9e9793fc55e31bc111
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-15 16:03:44 +0100

    added to-do component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 
+// Components
+import ToDos from "./Components/todos";
+
 class App extends Component {
+
+    state = {
+        todos: [
+            {id: 1, content: 'Buy some milk.'},
+            {id: 2, content: 'Play some mario kart.'},
+        ]
+    }
+
     render(){
         return (
-            <div>
+            <div className='App'>
+                <h1 className = 'center blue-text'> To Do's: </h1>
                 App.js Class
+                <ToDos {...this.state}/>
             </div>
         )
     }
---
+++ src/Components/todos.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ToDos = () => {
+    return (
+        <div className = 'todos collection'>
+            To Do Component
+        </div>
+    )
+}
+
+export default ToDos
