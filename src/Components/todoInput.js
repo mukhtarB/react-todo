02109ec4fdeaabcbdd827da94ce8c1e5362e2dbf
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-16 04:20:41 +0100

    added change Handler

--- src/Components/todoInput.js
+++ src/Components/todoInput.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 class InputField extends Component {
+
+    state = {
+        formContent: ''
+    }
+
+    changeHandler = (e) => {
+        this.setState({
+            formContent: e.target.value
+        })
+    }
+
     render (){
         return (
             <div>
                 <form>
                     <label>Add new To Do?</label>
-                    <input type = "text" />
+                    <input type = "text" onChange={this.changeHandler} />
+                    {console.log(this.state)}
                 </form>
             </div>
         )
