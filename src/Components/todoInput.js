b29d2c0d6067830ef8d36288753c5f27e5f95ff1
Mukhtar Abdulrazaq <mukhtar.b017@gmail.com>
2021-11-16 04:09:28 +0100

    created input component shell

---
+++ src/Components/todoInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class InputField extends Component {
+    render (){
+        return (
+            <div>
+                Input Field Component
+            </div>
+        )
+    }
+}
+
+export default InputField;
